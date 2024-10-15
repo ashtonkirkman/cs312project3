e81f5d21654b536d098c420a43e44dec5f24fd58
ashtonkirkman <akirkman@byu.edu>
2024-10-15 17:17:00 -0600

    Implemented find_shortest_path_with_array

--- network_routing.py
+++ network_routing.py
@@ -13,6 +13,7 @@ def find_shortest_path_with_heap(
     """
 
 
+
 def find_shortest_path_with_array(
         graph: dict[int, dict[int, float]],
         source: int,
@@ -26,3 +27,37 @@ def find_shortest_path_with_array(
         - the list of nodes (including `source` and `target`)
         - the cost of the path
     """
+    number_of_nodes = len(graph)
+
+    dist = [float('inf')] * number_of_nodes
+    prev = [None] * number_of_nodes
+    dist[source] = 0
+
+    # Create a list of nodes to visit
+    H = list(range(number_of_nodes))
+
+    while H:
+        u = min(H, key=lambda node: dist[node])
+        H.remove(u)
+
+        if dist[u] == float('inf') or u == target:
+            break
+
+        # for all edges (u, v) in E
+        for v, weight in graph[u].items():
+            if dist[v] > dist[u] + weight:
+                dist[v] = dist[u] + weight
+                prev[v] = u
+
+    path = []
+    current = target
+    if dist[target] == float('inf'):
+        return path, dist[target]
+
+    while current is not None:
+        path.insert(0, current)
+        current = prev[current]
+
+    return path, dist[target]
+
+
